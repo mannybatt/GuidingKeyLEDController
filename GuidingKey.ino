fc2147a0a9b77bd005864741dd68c4fe13166fb3
Manny Batt <55517248+mannybatt@users.noreply.github.com>
2024-11-19 04:46:26 -0500

    Added EEPROM to remember previous output!
    This makes it more useful to utilize when glitches happen, as the system will reboot into the previously last used LED output.

--- GuidingKey.ino
+++ GuidingKey.ino
@@ -3,13 +3,13 @@
 
 
 /**
- * 
- *  Designed to be the ultimate RGB controller, this code listens for MQTT commands
- *  that change the states of relay sending RGB data to a string of addressable 
- *  lights. It switches between PixelBlaze, WLED, and a custom Twinkle Pattern. 
- *    -Manny Batt
- * 
- */
+
+    Designed to be the ultimate RGB controller, this code listens for MQTT commands
+    that change the states of relay sending RGB data to a string of addressable
+    lights. It switches between PixelBlaze, WLED, and a custom Twinkle Pattern.
+      -Manny Batt
+
+*/
 
 
 // ***************************************
@@ -51,6 +51,7 @@ const char* password = STAPSK;
 #endif
 #define MQTT_KEEP_ALIVE 150
 unsigned long previousTime;
+float mqttConnectFlag = 0.0;
 
 //Initialize and Subscribe to MQTT
 WiFiClient client;
@@ -64,6 +65,9 @@ Adafruit_MQTT_Publish guidingKeyPub = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME 
 #define r_ground D5
 #define r_vcc D6
 
+//EEPROM
+#include <ESP_EEPROM.h>
+
 //RGB
 #include <Adafruit_NeoPixel.h>
 #define PIN D4
@@ -74,8 +78,8 @@ float blueStates[Pixels];
 float greenStates[Pixels];
 float fadeRate = 0.96;
 
-//System
-uint16_t state = 0; //0=pixelBlaze 1=WLed 2=twinkleStars
+//System Variables
+int state = 1; //0=pixelBlaze 1=WLed 2=twinkleStars
 uint16_t value = 0;
 uint16_t twinkleConstant = 20;
 
@@ -89,41 +93,58 @@ uint16_t twinkleConstant = 20;
 
 void setup() {
 
+  //EEPROM variable size initialization
+  EEPROM.begin(16);
+
+  /******* TO RESET CURRENT SONG UNCOMMENT BELOW *******/
+  //Place data into EEPROM
+  //EEPROM.put(1, state);  // int - so 4 bytes (next address is '4')
+  //Actually write that data to EEPROM
+  //boolean ok = EEPROM.commit();
+  //Serial.println((ok) ? "First commit OK" : "Commit failed");
+
+  //Load current song from EEPROM
+  EEPROM.get(0, state);
+
+  //Relays
   delay(100);
   pinMode(r_preData, OUTPUT);
   pinMode(r_data, OUTPUT);
   pinMode(r_ground, OUTPUT);
   pinMode(r_vcc, OUTPUT);
-  delay(100);                   //LOW = Not Activated
-  digitalWrite(r_preData, LOW); //LOW = TwinkleStars   HIGH = WLed
-  digitalWrite(r_data, LOW);    //LOW = preData   HIGH = PixelBlaze
-  digitalWrite(r_ground, LOW);
-  digitalWrite(r_vcc, LOW);
+  delay(100);
+  if (state == 0) {
+    toPixelBlaze();
+  }
+  else if (state == 1) {
+    toWLed();
+  }
+  else if (state == 2) {
+    toTwinkleStars();
+  }
 
+  //Setup Adafruit Neopixel (Twinkling)
   strip.begin();
   strip.show();
-  
-  for(uint16_t l = 0; l < Pixels; l++) {
+  for (uint16_t l = 0; l < Pixels; l++) {
     redStates[l] = 0;
     greenStates[l] = 0;
     blueStates[l] = 0;
   }
 
-  //Initialize Serial
-  Serial.begin(115200);
-  Serial.println("Booting");
-
   //Initialize Stars
   digitalWrite(r_vcc, HIGH);
-  toWLed();
 
   //Initialize WiFi & OTA
   wifiSetup();
 
   //Initialize MQTT
   mqtt.subscribe(&guidingKeySub);
-
+  MQTT_connect();
+  delay(1500);
+  guidingKeyPub.publish(state);
   Serial.println("***** May your heart be your guiding key *****");
+
 }
 
 
@@ -146,72 +167,74 @@ void loop() {
     uint16_t value = atoi((char *)guidingKeySub.lastread);
     state = value;
     delay(250);
+    if (state == 0 || state == 1 || state == 2) {
+      //Store into EEPROM
+      EEPROM.put(0, state);
+      boolean ok = EEPROM.commit();
+      Serial.println((ok) ? "First commit OK" : "Commit failed");
+    }
+    //Implement States
+    // [PixelBlaze]
+    if (state == 0) {
+      toPixelBlaze();
+    }
+    // [WLed]
+    else if (state == 1) {
+      toWLed();
+    }
+    // [TwinkleStars]
+    else if (state == 2) {
+      toTwinkleStars();
+    }
+    // [Lower TwinkleConstant]
+    else if (state == 3) {
+      if (twinkleConstant > -1) {
+        twinkleConstant-=2;
+      }
+    }
+    // [Raise TwinkleConstant]
+    else if (state == 4) {
+      if (twinkleConstant < 41) {
+        twinkleConstant+=2;
+      }
+    }
   }
 
-  //Implement States
-  // [PixelBlaze]
-  if (state == 0) {
-    toPixelBlaze();
-  }
-  // [WLed]
-  else if (state == 1) {
-    toWLed();
-  }
-  // [TwinkleStars]
-  else if (state == 2) {
-    toTwinkleStars();
-  }
-  // [Lower TwinkleConstant]
-  else if (state == 3) {
-    if(twinkleConstant > -1){
-      twinkleConstant--;
+
+  //Twinkling Lights Pattern
+  if (random(twinkleConstant) == 1) {
+    int i = random(Pixels);
+    if (redStates[i] < 1 && greenStates[i] < 1 && blueStates[i] < 1) {
+      redStates[i] = random(256);
+      greenStates[i] = random(256);
+      blueStates[i] = random(256);
     }
   }
-  // [Raise TwinkleConstant]
-  else if (state == 4) {
-    if(twinkleConstant < 41){
-      twinkleConstant++;
-    }
-  }
-  else{
-    if (random(twinkleConstant) == 1) {
-      int i = random(Pixels);
-      if (redStates[i] < 1 && greenStates[i] < 1 && blueStates[i] < 1) {
-        redStates[i] = random(256);
-        greenStates[i] = random(256);
-        blueStates[i] = random(256);
-      }
-    }
-    
-    for(int l = 0; l < Pixels; l++) {
-      if (redStates[l] > 1 || greenStates[l] > 1 || blueStates[l] > 1) {
-        strip.setPixelColor(l, redStates[l], greenStates[l], blueStates[l]);
-        
-        if (redStates[l] > 1) {
-          redStates[l] = redStates[l] * fadeRate;
-        } else {
-          redStates[l] = 0;
-        }
-        
-        if (greenStates[l] > 1) {
-          greenStates[l] = greenStates[l] * fadeRate;
-        } else {
-          greenStates[l] = 0;
-        }
-        
-        if (blueStates[l] > 1) {
-          blueStates[l] = blueStates[l] * fadeRate;
-        } else {
-          blueStates[l] = 0;
-        }
-        
+
+  for (int l = 0; l < Pixels; l++) {
+    if (redStates[l] > 1 || greenStates[l] > 1 || blueStates[l] > 1) {
+      strip.setPixelColor(l, redStates[l], greenStates[l], blueStates[l]);
+      if (redStates[l] > 1) {
+        redStates[l] = redStates[l] * fadeRate;
       } else {
-        strip.setPixelColor(l, 0,0,0);
+        redStates[l] = 0;
       }
+      if (greenStates[l] > 1) {
+        greenStates[l] = greenStates[l] * fadeRate;
+      } else {
+        greenStates[l] = 0;
+      }
+      if (blueStates[l] > 1) {
+        blueStates[l] = blueStates[l] * fadeRate;
+      } else {
+        blueStates[l] = 0;
+      }
+    } else {
+      strip.setPixelColor(l, 0, 0, 0);
     }
-    strip.show();
-    delay(1); //Change this value to determine the speed of the pattern  
   }
+  strip.show();
+  delay(1); //Change this value to determine the speed of the pattern
 }
 
 
@@ -223,10 +246,11 @@ void loop() {
 
 
 void toPixelBlaze() {
+
   Serial.println("toPixelBlaze");
-  state = -1;
+  //state = -1;
   ledReset();
-  delay(500);
+  //delay(500);
   digitalWrite(r_preData, LOW); //LOW = TwinkleStars   HIGH = WLed
   digitalWrite(r_data, HIGH);    //LOW = preData   HIGH = PixelBlaze
   digitalWrite(r_ground, HIGH);
@@ -234,10 +258,11 @@ void toPixelBlaze() {
 }
 
 void toWLed() {
+
   Serial.println("toWLed");
-  state = -1;
+  //state = -1;
   ledReset();
-  delay(500);
+  //delay(500);
   digitalWrite(r_preData, HIGH); //LOW = TwinkleStars   HIGH = WLed
   digitalWrite(r_data, LOW);    //LOW = preData   HIGH = PixelBlaze
   digitalWrite(r_ground, HIGH);
@@ -245,10 +270,11 @@ void toWLed() {
 }
 
 void toTwinkleStars() {
+
   Serial.println("toTwinkleStars");
-  state = -1;
+  //state = -1;
   ledReset();
-  delay(500);
+  delay(100);
   digitalWrite(r_preData, LOW); //LOW = TwinkleStars   HIGH = WLed
   digitalWrite(r_data, LOW);    //LOW = preData   HIGH = PixelBlaze
   digitalWrite(r_ground, HIGH);
@@ -256,12 +282,51 @@ void toTwinkleStars() {
 }
 
 void ledReset() {
+
+  delay(100);
   digitalWrite(r_ground, LOW);
   digitalWrite(r_vcc, LOW);
 }
 
+void MQTT_connect() {
+
+  int8_t ret;
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    if (mqttConnectFlag == 0) {
+      //Serial.println("Connected");
+      mqttConnectFlag++;
+    }
+    return;
+  }
+  Serial.println("Connecting to MQTT... ");
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      //while (1);
+      Serial.println("Wait 5 secomds to reconnect");
+      delay(5000);
+    }
+  }
+}
+
 void wifiSetup() {
 
+  //Serial
+  Serial.begin(115200);
+  delay(300);
+  Serial.println();
+  Serial.println();
+  Serial.println("****************************************");
+  Serial.println("Booting");
+
+  //WiFi and OTA
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -269,7 +334,7 @@ void wifiSetup() {
     delay(5000);
     ESP.restart();
   }
-  ArduinoOTA.setHostname("GuidingKey");
+  ArduinoOTA.setHostname("GuidingKey");                                                          /** TODO **/
   ArduinoOTA.onStart([]() {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
@@ -303,31 +368,4 @@ void wifiSetup() {
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-}
-
-void MQTT_connect() {
-  int8_t ret;
-
-  // Stop if already connected.
-  if (mqtt.connected()) {
-    //Serial.println("Connected");
-    return;
-  }
-  Serial.print("Connecting to MQTT... ");
-  uint8_t retries = 3;
-
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-    Serial.println(mqtt.connectErrorString(ret));
-    Serial.println("Retrying MQTT connection in 5 seconds...");
-    mqtt.disconnect();
-    delay(5000);  // wait 5 seconds
-    retries--;
-    if (retries == 0) {
-      // basically die and wait for WDT to reset me
-      //while (1);
-      Serial.println("Wait 10 min to reconnect");
-      delay(600000);
-    }
-  }
-  Serial.println("MQTT Connected!");
 }
